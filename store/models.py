66ef19bf69f23b22a1cde063a9d64f8ae68bb6f2
Mohammad <mamad.jafari91@gmail.com>
2021-09-28 01:30:15 +0330

    Add ProductModel

--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ProductModel(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1023, blank=True)
+    rate = models.PositiveSmallIntegerField(default=0)
+    number_of_rates = models.PositiveIntegerField(default=0)
+    is_available = models.BooleanField(default=False)
